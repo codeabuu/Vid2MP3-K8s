e9471c03d286d634c837a65557c35aa3ea62ccd8
codeabuu <abdulngui718@gmail.com>
2024-08-21 16:38:28 -0400

    Add Converter Service for Video-to-MP3 Conversion with Minikube Deployment
    - Implemented consumer.py to handle video-to-MP3 conversion using MongoDB and GridFS.
    - Established RabbitMQ connection for processing video files from the message queue.
    - Integrated video and MP3 storage in MongoDB using separate databases and GridFS collections.
    - Configured message acknowledgment in the RabbitMQ consumer based on conversion success.
    - Created Kubernetes Deployment manifest for the converter service, including ConfigMap and Secret references.
    - Configured the converter service deployment in Minikube with RollingUpdate strategy and increased replica count for better scalability.

--- python/src/converter/consumer.py
+++ python/src/converter/consumer.py
@@ -1,21 +1,19 @@
 import pika, sys, os, time
-import pika.connection
 from pymongo import MongoClient
 import gridfs
 from convert import to_mp3
+
 
 def main():
     client = MongoClient("host.minikube.internal", 27017)
     db_videos = client.videos
     db_mp3s = client.mp3s
-
+    # gridfs
     fs_videos = gridfs.GridFS(db_videos)
     fs_mp3s = gridfs.GridFS(db_mp3s)
 
-    #rabbitmq con
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="rabbitmq")
-    )
+    # rabbitmq connection
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     channel = connection.channel()
 
     def callback(ch, method, properties, body):
@@ -26,19 +24,18 @@ def main():
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
     channel.basic_consume(
-        queue=os.environ.get("VIDEO_QUEUE"),
-        on_message_callback=callback
+        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
     )
+
     print("Waiting for messages. To exit press CTRL+C")
 
     channel.start_consuming()
-
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("Interupted!")
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
